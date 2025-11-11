1bdb258257e24b94cf7703168b8cfc632715506c
dressupdarling <yevgenyokun2@gmail.com>
2025-11-11 20:42:02 +0000

    Update Distance_calculator.py

--- Distance_calculator.py
+++ Distance_calculator.py
@@ -22,26 +22,23 @@ totalDistance = 0.0
 def convertToMPS(pMilesPerHour):
   speedKmPerHour = pMilesPerHour * KILOMETERS_PER_MILE
   speedMPerSecond = (speedKmPerHour * 1000) / (60 * 60)
-  
-  # ====> Return the speed in metres per second
-  return (              )
+  return speedMPerSecond
 
 # -------------------------------------
 # Main program
 # -------------------------------------
-speedMilesPerHour = float(input("Enter car speed in miles per hour (e.g. 60)"))
-seconds = int(input("How many seconds: (e.g. 10)"))
+speedMilesPerHour = float(input("Enter car speed in miles per hour (e.g. 60): "))
+seconds = int(input("How many seconds: (e.g. 10): "))
 
-# ====> Call the user defined subprogram
-speedMetresPerSecond =              (speedMilesPerHour)
+speedMetresPerSecond = convertToMPS(speedMilesPerHour)
 
 totalDistance = 0
 print("=" * 37)
 print("| Time (s) | Distance travelled (m) |")
 print("=" * 37)
 
-for i in range(seconds + 1):
-  print("|{:^10}|{:^24.2f}|".format(totalDistance, i))
+for i in range(seconds+1):
+  print("|{:^10}|{:^24.2f}|".format(i, totalDistance))
   totalDistance += speedMetresPerSecond
-  
+
 print("=" * 37)
